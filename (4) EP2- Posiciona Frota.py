e6ce965c44caa9a44dd8544409e4beb28f8057bd
carolinasilverio <carolinafms@al.insper.edu.br>
2023-05-03 14:57:15 -0300

    Posiciona Frota

---
+++ (4) EP2- Posiciona Frota.py
@@ -0,0 +1,21 @@
+def posiciona_frota(frota):
+    tabuleiro = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ]
+    for tipo_navio, listas_posicoes in frota.items():
+        for navio in listas_posicoes:
+            for posicao in navio:
+                linha = posicao[0]
+                coluna = posicao[1]
+                tabuleiro[linha][coluna] = 1
+    
+    return tabuleiro
